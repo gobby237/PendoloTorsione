add246fd2b5fcaa1acf6ce9de5075a7740c3cca6
tony <antonio.gobbato@studenti.unipd.it>
2025-06-02 11:17:31 +0200

    codice aggiornato con tf, a_max, wf e relative incertezze corrette

--- analisi_dati/analisi_secondo_giorno/analisi_picchi_fit_tabella.cpp
+++ analisi_dati/analisi_secondo_giorno/analisi_picchi_fit_tabella.cpp
@@ -201,61 +201,7 @@ int main ()
             }
         }
 
-        inRegion = false; 
-        for (int i = 0; i < (int)a.size(); i++) {
-            if (!inRegion && i < (int)a.size() && a.at(i) > thr) {
-                inRegion = true;
-                start = i;
-            }
-            else if (inRegion && (a.at(i) <= thr)) {
-                int end = i - 1;
-                int m = end - start + 1;
-                if (m >= 3) {
-                    // somma per minimi quadrati
-                    double S0 = m, S1=0,S2=0,S3=0,S4=0;
-                    double Sy=0,Sxy=0,Sx2y=0;
-                    for (int j = start; j <= end; j++) {
-                        double x = t.at(j);
-                        double y = a.at(j);
-                        double x2 = x*x;
-                        S1   += x;
-                        S2   += x2;
-                        S3   += x2 * x;
-                        S4   += x2 * x2;
-                        Sy   += y;
-                        Sxy  += x * y;
-                        Sx2y += x2 * y;
-                    }
-                    // sistema normale 3x3 aumentato
-                    double M[3][4] = {
-                        {S4,  S3,  S2,   Sx2y},
-                        {S3,  S2,  S1,   Sxy },
-                        {S2,  S1,  S0,   Sy  }
-                    };
-                    // gauss
-                    for (int r = 0; r < 3; r++) {
-                        double piv = M[r][r];
-                        if (fabs(piv) < 1e-12) break;
-                        for (int c = r; c < 4; ++c) M[r][c] /= piv;
-                        for (int rr = r+1; rr < 3; ++rr) {
-                            double f = M[rr][r];
-                            for (int c = r; c < 4; ++c)
-                                M[rr][c] -= f * M[r][c];
-                        }
-                    }
-                    // risolvo a ritroso
-                    double c = M[2][3];
-                    double b = M[1][3] - M[1][2]*c;
-                    double a_coef = M[0][3] - M[0][1]*b - M[0][2]*c;
-                    // vertice parabola
-                    double x_peak = -b/(2*a_coef);
-                    double y_peak = a_coef*x_peak*x_peak + b*x_peak + c;
-                    t_max.push_back(x_peak);
-                    max.push_back(y_peak);
-                }
-                inRegion = false;
-            }
-        }
+        
 
         
         // abbiamo trovato tutti i massimi ed i tempi  
