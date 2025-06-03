701f87c09e0f91206610df9ead81f4619c7fbc8f
tony <antonio.gobbato@studenti.unipd.it>
2025-06-03 10:57:49 +0200

    dati per regressione check_max_rad.txt

---
+++ analisi_dati/analisi_secondo_giorno/smorzamento/giri_to_rad.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <cmath>
+using namespace std; 
+int main ()
+{
+    ifstream fin ("check_max.txt"); 
+
+    ofstream out ("check_max_rad.txt", std::ios::app); 
+
+    double val1, val2; 
+
+    vector<double> y, x; 
+
+
+    while (fin>>val1>>val2)
+    {
+        y.push_back(val2);
+        x.push_back(val1);  
+    }
+
+    for (int i = 0; i < y.size(); i ++ )
+    {
+        out << x.at(i) << " " << log(y.at(i)*2*M_PI) << "\n"; 
+    }
+
+    
+    return 0; 
+}
